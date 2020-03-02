e67ffdda09c68c370ad9f4c8db685db4396028c4
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-03-02 20:57:29 +0800

    added the homepage for the admin side

--- admin/index.php
+++ admin/index.php
@@ -1,21 +1,26 @@
 <?php
+    session_start();
     include_once '../dbconnect.php';
     
-    require_once '../Classes/PHPExcel.php';
+//verfying admin login - only accounttype allowed is "Admin" 
+    if(isset($_POST['btnAdminLogin'])){
+        if(isset($_POST['adminuser']) && isset($_POST['adminpass'])){
+            $txtuser = $_POST['adminuser'];
+            $txtpass = $_POST['adminpass'];
 
-      // echo $txtGrade, $txtSection;
-
-      include_once "../Classes/PHPExcel.php";
-
-      $tmpfname = '../test.xlsx';
-          $excelReader = PHPExcel_IOFactory::createReaderForFile($tmpfname);
-          //$excelReader->setPreCalculateFormulas(true);
-          $excelObj = $excelReader->load($tmpfname);
-         // $worksheet = $excelObj->setPreCalculateFormulate(true);
-          //$worksheet->setPreCalculateFormulas(true);
-         $worksheet = $excelObj->getSheet(0);
-          //$worksheet->setPreCalculateFormulas(true);
-
+            $qryVerifyAdmin = "select * from users where username='$txtuser' and password='$txtpass' and accounttype='Admin' ";
+            $execQuery = mysqli_query($db,$qryVerifyAdmin);
+            $response = mysqli_num_rows($execQuery);
+            if($response>0){
+                $_SESSION['loggedinadmin'] = $txtuser;
+                echo "<script type='text/javascript'>alert('Admin Access Granted!');</script>";   
+                header("Location: dashboard.php");
+            }
+            else{
+                echo "<script type='text/javascript'>alert('Admin Access Denied!');</script>";   
+            }
+        }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -30,170 +35,101 @@
 <body>
     
 
-<nav style="margin-top:0%" class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="/consolidated/excel/index.php">Consolidated App</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+<nav class="navbar navbar-dark bg-dark">
+<a class="navbar-brand" href="#">Consolidated App</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav ml-auto">
-    <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Welcome, <?php echo $_SESSION['loggedin']?>
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-        <a class="dropdown-item" href="edit.php">Edit Account</a>
-        <a class="dropdown-item" href="delete.php">Delete Account</a>
-          <a class="dropdown-item" href="logout.php">Logout</a>
-        </div>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Team</a>
       </li>
     </ul>
   </div>
+
 </nav>
 
-<?php 
-     $lastRow = $worksheet->getHighestRow();
-     echo' <table class="table">';
-      for ($row = 1; $row <= $lastRow; $row++) {
-              $a = $worksheet->getCell('A'.$row)->getValue();
-              $b = $worksheet->getCell('B'.$row)->getValue();
-              $c =  $worksheet->getCell('C'.$row)->getValue();
-              $d =  $worksheet->getCell('D'.$row)->getValue();
-              $e =  $worksheet->getCell('E'.$row)->getValue();
-              $f =  $worksheet->getCell('F'.$row)->getValue();
-              $h =  $worksheet->getCell('H'.$row)->getValue();
-
-              echo '<tr><td>';
-              echo $a;
-              echo '</td></td>';
-
-              echo '<td><td>';
-              echo $b;
-              echo '</td></td';
-
-              echo '<td><td>';
-              echo $c;
-              echo '</td></td';
-
-              echo '<td><td>';
-              echo $d;
-              echo '</td></td';
-
-              echo '<td><td>';
-              echo $e;
-              echo '</td></td';
-
-              echo '<td><td>';
-              echo $f;
-              echo '</td></td';
-
-              echo '<td><td>';
-              echo $h;
-              
-              echo '</td></tr>';
-      }
-    echo' </table>';
-
-?>
-<div class="container">
-<h1 style="text-transform:uppercase" class="text-center">
-      Admin Contoller
-</h1>
-<p style="color:grey" class="grey-text text-center">
-      Below are the control access the admin has to the teachers
-</p>
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Teacher</th>
-      <th scope="col">Section</th>
-      <th scope="col">Grade Handled</th>
-      <th scope="col">Action</th>
-      <th scope="col">Alter</th>
-      <th scope="col">Control</th>
-    </tr>
-  </thead>
-  <tbody>
-  <form action="index.php" method="get">
-  <?php
-    $qry = "select * from file";
-    $result = mysqli_query($db,$qry);
-
-    if (mysqli_num_rows($result) > 0) {
-        // output data of each row
-        while($row = mysqli_fetch_assoc($result)) {
-            //echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
-            $id = $row['id'];
-            $name = $row['teacher'];
-            $section = $row['section'];
-            $grade = $row['grade'];
-            $filename = $row['filedir'];
-
-    echo "<tr>";
-      echo "<input hidden value='$id'/>";
-      echo "<th scope='row'>$id</th>";
-      echo "<td>$name</td>";
-      echo "<td>$section</td>";
-      echo "<td>$grade</td>";
-      echo '<td><button 
-      name="btnView"
-      class="btn btn-sm btn-primary">
-      view
-      </button></td>';
-      echo "<td>
-            <button 
-            name='btnEdit'
-            class='btn btn-sm btn-success'>
-                edit
-            </button>
-      </td>";
-      echo '<td><button 
-      name="btnDelete"
-      class="btn btn-sm btn-danger">
-      delete
-      </button></td>
-    </tr>';
-    }
-    } else {
-        echo "0 results";
-    }
-    ?>
-
-  </tbody>
-  </form>
-</table>
-<?php
-     
-     if(isset($_GET['btnView'])){
-        echo "<script type='text/javascript'>alert('Button View');</script>";   
-    }
-    
-    if(isset($_GET['btnEdit'])){
-        echo "<script type='text/javascript'>alert('Button Edit');</script>";   
-    }
-    
-    if(isset($_GET['btnDelete'])){
-        //$qryDelete = "delete from file id= '' ";
-        echo "<script type='text/javascript'>alert('Button Deleted'$id);</script>";   
-    }
-?>
-
-    <div class="container">
+<div style="margin-top:10%;margin-bottom:10%" class="container">
+    <div style="width:50%" class="jumbotron">
+    <h3 class="text-center">Admin Authentication</h3>
+        <form method="POST" action="">
+            <div class="form-group">
+                <label for="exampleInputEmail1">Admin Access Key</label>
+                <input 
+                type="text" 
+                name="adminuser"
+                class="form-control" 
+                id="exampleInputEmail1" 
+                aria-describedby="emailHelp">
+                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input 
+                type="password" 
+                class="form-control" 
+                name="adminpass"
+                id="exampleInputPassword1">
+            </div>
+            <div class="form-group form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            </div>
+            <button type="submit" name="btnAdminLogin" class="btn btn-primary">Submit</button>
+        </form>
     </div>
 </div>
-
 <!-- 
+<center>
+<div class="container" style="margin-top:10%">
+    <div style="padding:25px;width:50%" class="card">
+            <form method="POST" action="index.php">
+            <h3 class="text-center">Admin Authentication</h3>
+            <div class="form-group">
+                <label for="exampleInputEmail1">Username</label>
+                <input 
+                type="text"
+                class="form-control" 
+                id="exampleInputEmail1"
+                name="username"
+                aria-describedby="emailHelp">
+                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" 
+                name="password"
+                class="form-control" id="exampleInputPassword1">
+            </div>
+            <div class="form-group form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Remember my password</label>
+            </div>
+            <button name="btnLogin" type="submit" class="btn btn-primary">Login</button>
+            </form>
+        </div>
+</div>
+</center> -->
+
+</div>
+
+
 <div class="footer bg-dark">
     <div>
         <a style="color:white" href="#footerlink">Copyright 2020</a>
     </div>
-</div> -->
+</div>
 
 <style>
 .footer {
    left: 0;
-   position: fixed;
    padding:15px;
    bottom: 0;
    width: 100%;
